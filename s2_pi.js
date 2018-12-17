26bc7101bb9b7e69f79b6b2e97d8e25c9ef59fb4
jpcote <jpcote@kinova.ca>
2018-12-17 13:17:39 -0500

    initial commit

---
+++ s2_pi.js
@@ -0,0 +1,299 @@
+/**
+ Copyright (c) 2016, 2017 Alan Yorinks All right reserved.
+
+ Python Banyan is free software; you can redistribute it and/or
+ modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ Version 3 as published by the Free Software Foundation; either
+ or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+(function (ext) {
+    var socket = null;
+
+    var connected = false;
+
+    // an array to hold possible digital input values for the reporter block
+    var digital_inputs = new Array(32);
+    var myStatus = 1; // initially yellow
+    var myMsg = 'not_ready';
+
+    ext.cnct = function (callback) {
+        window.socket = new WebSocket("ws://127.0.0.1:9000");
+        window.socket.onopen = function () {
+            var msg = JSON.stringify({
+                "command": "ready"
+            });
+            window.socket.send(msg);
+            myStatus = 2;
+
+            // change status light from yellow to green
+            myMsg = 'ready';
+            connected = true;
+
+            // initialize the reporter buffer
+            digital_inputs.fill('0');
+
+            // give the connection time establish
+            window.setTimeout(function() {
+            callback();
+        }, 1000);
+
+        };
+
+        window.socket.onmessage = function (message) {
+            var msg = JSON.parse(message.data);
+
+            // handle the only reporter message from the server
+            // for changes in digital input state
+            var reporter = msg['report'];
+            if(reporter === 'digital_input_change') {
+                var pin = msg['pin'];
+                digital_inputs[parseInt(pin)] = msg['level']
+            }
+            console.log(message.data)
+        };
+        window.socket.onclose = function (e) {
+            console.log("Connection closed.");
+            socket = null;
+            connected = false;
+            myStatus = 1;
+            myMsg = 'not_ready'
+        };
+        
+        window.socket.onerror = function (e) {
+            console.log("Connection closed.");
+            socket = null;
+            connected = false;
+            myStatus = 1;
+            myMsg = 'not_ready'
+            callback();
+        };    
+    };
+	
+	// Connect to Arm
+    ext.connectToArm = function (callback) {
+        var msg = JSON.stringify({
+            "command": "connectToArm"
+        });
+
+        console.log("Begin more ConnectToArm")
+        window.socket.send(msg);
+        
+        console.log("Begin ConnectToArm")
+        
+        window.setTimeout(function() {
+            callback();
+        }, 2000);
+        
+        console.log("After ConnectToArm")
+    };
+	// MoveToPosition
+    ext.moveToPosition = function (callback) {
+        var msg = JSON.stringify({
+            "command": "moveToPosition"
+        });
+        window.socket.send(msg);
+        
+        callback();
+    };
+    
+        // when the digital write block is executed
+    ext.moveToPosition2 = function (angle1, angle2, angle3, angle4, angle5, angle6, callback) {
+        console.log("moveToPosition2");
+        var msg = JSON.stringify({
+            "command": 'moveToPosition2', 'angle1': angle1, 'angle2': angle2, 'angle3': angle3, 'angle4': angle4, 'angle5': angle5, 'angle6': angle6
+        });
+        console.log(msg);
+        window.socket.send(msg);
+        
+        callback();
+    };
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function () {
+        var msg = JSON.stringify({
+            "command": "shutdown"
+        });
+        window.socket.send(msg);
+    };
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function (status, msg) {
+        return {status: myStatus, msg: myMsg};
+    };
+
+    // when the connect to server block is executed
+    ext.input = function (pin) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        // validate the pin number for the mode
+        if (validatePin(pin)) {
+            var msg = JSON.stringify({
+                "command": 'input', 'pin': pin
+            });
+            window.socket.send(msg);
+        }
+    };
+
+    // when the digital write block is executed
+    ext.digital_write = function (pin, state) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        console.log("digital write");
+        // validate the pin number for the mode
+        if (validatePin(pin)) {
+            var msg = JSON.stringify({
+                "command": 'digital_write', 'pin': pin, 'state': state
+            });
+            console.log(msg);
+            window.socket.send(msg);
+        }
+    };
+
+    // when the PWM block is executed
+    ext.analog_write = function (pin, value) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        console.log("analog write");
+        // validate the pin number for the mode
+        if (validatePin(pin)) {
+            // validate value to be between 0 and 255
+            if (value === 'VAL') {
+                alert("PWM Value must be in the range of 0 - 255");
+            }
+            else {
+                value = parseInt(value);
+                if (value < 0 || value > 255) {
+                    alert("PWM Value must be in the range of 0 - 255");
+                }
+                else {
+                    var msg = JSON.stringify({
+                        "command": 'analog_write', 'pin': pin, 'value': value
+                    });
+                    console.log(msg);
+                    window.socket.send(msg);
+                }
+            }
+        }
+    };
+    // ***Hackeduca --> when the Servo block is executed
+    ext.servo = function (pin, value) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        console.log("servo");
+        // validate the pin number for the mode
+        if (validatePin(pin)) {
+            // validate value to be between 0° and 180°
+            if (value === 'VAL') {
+                alert("Servo Value must be in the range of 0° - 180°");
+            }
+            else {
+                value = parseInt(value);
+                if (value < 0 || value > 180) {
+                    alert("Servo Value must be in the range of 0° - 180°");
+                }
+                else {
+                    var msg = JSON.stringify({
+                        "command": 'servo', 'pin': pin, 'value': value
+                    });
+                    console.log(msg);
+                    window.socket.send(msg);
+                }
+            }
+        }
+    };
+	
+    // when the play tone block is executed
+    ext.play_tone = function (pin, frequency) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        // validate the pin number for the mode
+        if (validatePin(pin)) {
+            var msg = JSON.stringify({
+                "command": 'tone', 'pin': pin, 'frequency': frequency
+            });
+            console.log(msg);
+            window.socket.send(msg);
+        }
+    };
+
+    // when the digital read reporter block is executed
+    ext.digital_read = function (pin) {
+        if (connected == false) {
+            alert("Server Not Connected");
+        }
+        else {
+                return digital_inputs[parseInt(pin)]
+
+        }
+    };
+
+    ext.socket_state_read = function (state) {
+        return window.socket.readyState
+    };   
+    
+    ext.socket_buffered_amount = function (amount) {
+        return window.socket.bufferedAmount
+    };  
+
+    
+    
+
+    // general function to validate the pin value
+    function validatePin(pin) {
+        var rValue = true;
+        if (pin === 'PIN') {
+            alert("Insert a valid BCM pin number.");
+            rValue = false;
+        }
+        else {
+            var pinInt = parseInt(pin);
+            if (pinInt < 0 || pinInt > 31) {
+                alert("BCM pin number must be in the range of 0-31.");
+                rValue = false;
+            }
+        }
+        return rValue;
+    }
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            ["w", 'Connect to s2_pi server.', 'cnct'],
+            ["w", 'Connect to Jaco2 Arm.', 'connectToArm'],            
+            ["w", 'MoveToPosition', 'moveToPosition'],
+            ["w", 'MoveToPosition2- Angle1: %n, Angle2: %n, Angle3: %n, Angle4: %n, Angle5: %n, Angle6: %n', 'moveToPosition2', "0.0", "0.0", "0.0", "0.0", "0.0", "0.0"],
+            [" ", 'Set BCM %n as an Input', 'input','PIN'],
+            [" ", "Set BCM %n Output to %m.high_low", "digital_write", "PIN", "0"],
+            [" ", "Set BCM PWM Out %n to %n", "analog_write", "PIN", "VAL"],
+            [" ", "Set BCM %n as Servo with angle = %n (0° - 180°)", "servo", "PIN", "0"],     // ***Hackeduca --> Block for Servo 			
+            [" ", "Tone: BCM %n HZ: %n", "play_tone", "PIN", 1000],
+            ["r", "Read Digital Pin %n", "digital_read", "PIN"],            
+            ["r", "Socket state %n", "socket_state_read", "state"],            
+            ["r", "Socket bufferedAmount %n", "socket_buffered_amount", "amount"]
+
+        ],
+        "menus": {
+            "high_low": ["0", "1"]
+
+        },
+        url: 'http://MrYsLab.github.io/s2-pi'
+    };
+
+    // Register the extension
+    ScratchExtensions.register('s2_pi', descriptor, ext);
+})({});
